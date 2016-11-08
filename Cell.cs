6bdd75a983cec40059dda5ccf7bdd0aab7434552
rus100 <rus100@yandex.ru>
2016-11-09 00:58:45 +0700

    Create Cell.cs

---
+++ Cell.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake1
+{
+    class Cell
+    {
+       public int x,y;
+       public string put = "";
+       public Cell() {
+          
+        }
+
+    }
+    }
