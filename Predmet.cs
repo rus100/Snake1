a45ed59985a2250cb6d67e986187b8777480f188
rus100 <rus100@yandex.ru>
2016-11-09 01:02:10 +0700

    Create Predmet.cs

---
+++ Predmet.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake1
+{
+    class Predmet
+    {
+        public int x,y;
+       public int number;
+       public Predmet() { 
+       
+       
+       }
+
+    }
+}
